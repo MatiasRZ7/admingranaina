37a4b2dd44916ab3d5da236d58d62ee6fa5180de
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-05-09 15:46:26 +0200

    fixed error with url

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -3,9 +3,12 @@ import { columns } from "@/components/orderItems/OrderItemsColums";
 import React from "react";
 
 const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
-  const res = await fetch(`http://localhost:3000/api/orders/${params.orderId}`);
+  const res = await fetch(
+    `${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`
+  );
   const { orderDetails, customer } = await res.json();
-  const { street, city, state, postalCode, country } = orderDetails.shippingAddress;
+  const { street, city, state, postalCode, country } =
+    orderDetails.shippingAddress;
   return (
     <div className="flex flex-col p-10 gap-5">
       <p className="text-base-bold">
@@ -31,7 +34,11 @@ const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
         <span className="text-base-medium"> {orderDetails.shippingRate}</span>
       </p>
 
-      <DataTable columns={columns} data={orderDetails.products} searchKey="product" />
+      <DataTable
+        columns={columns}
+        data={orderDetails.products}
+        searchKey="product"
+      />
     </div>
   );
 };
